3f49216c796becc239d136e65c2e97d5ea685138
Max Seiden <mhseiden@umich.edu>
2012-09-18 19:35:31 -0400

    The are great changes. They really bring out the functionality of the product.

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ name = raw_input("What is your name? ")
 print "Hello world"
 print "line 2!"
 print name
+
+fib = lambda n:reduce(lambda x,n:[x[1],x[0]+x[1]], range(n),[0,1])[0]
+
+fib_val = raw_input("What Fibonacci number would you like to compute? ")
+print fib(int(fib_val))
